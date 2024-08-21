e7ffb3c4a5554b79bd1430e44976bacf003dd9d7
wakame-tech <kamata1919@gmail.com>
2024-08-22 01:20:41 +0900

    add: prisma client

--- api/test/client.ts
+++ api/test/client.ts
@@ -1,13 +1,16 @@
 import { AppRouter } from "../src/index.ts";
 import { createTRPCProxyClient, httpLink } from "@trpc/client";
 
+const url = "http://localhost:1234/trpc";
+// const url = "http://botcast.deno.dev/trpc";
+
 const client = createTRPCProxyClient<AppRouter>({
     links: [
         httpLink({
-            url: "http://localhost:1234/trpc",
+            url,
         }),
     ],
 });
 
-const res = await client.ping.query();
-console.log(JSON.stringify(res));
+const users = await client.users.query();
+console.log(JSON.stringify(users));
