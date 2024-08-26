b72e86be367996c645efba9e152d20a1500b02ae
wakame-tech <kamata1919@gmail.com>
2024-08-27 02:30:03 +0900

    clean: worker

--- worker/src/task.rs
+++ worker/src/task.rs
@@ -1,82 +1,93 @@
-use crate::{
-    scrape::{narou::Narou, EpisodeScraper},
-    synthesizer::{
-        concat_wavs,
-        voicevox::{VoiceVox, VoiceVoxSpeaker},
-        Synthesizer,
-    },
-    DB,
-};
-use scraper::Html;
-use std::{path::PathBuf, time::Duration};
-use surrealdb::opt::RecordId;
+use crate::{scrape::ScrapeEpisode, synthesis::Synthesis};
+use std::fmt::Debug;
+use surrealdb::{engine::local::Db, opt::RecordId, Surreal};
 use uuid::Uuid;
 
+pub(crate) trait RunTask
+where
+    Self: Debug + Clone + serde::Serialize + for<'de> serde::Deserialize<'de>,
+{
+    fn id(&self) -> &RecordId;
+
+    async fn run(&mut self) -> anyhow::Result<Option<Task>>;
+}
+
 #[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
-pub(crate) struct Task {
-    pub(crate) id: RecordId,
-    pub(crate) url: String,
+#[serde(tag = "type")]
+pub(crate) enum Task {
+    Scrape(ScrapeEpisode),
+    Synthesis(Synthesis),
 }
 
-impl Task {
-    pub(crate) fn new(url: String) -> Self {
-        Self {
-            id: RecordId::from(("task".to_string(), Uuid::new_v4().to_string())),
-            url,
+impl RunTask for Task {
+    fn id(&self) -> &RecordId {
+        match self {
+            Self::Scrape(task) => &task.id,
+            Self::Synthesis(task) => &task.id,
+        }
+    }
+
+    async fn run(&mut self) -> anyhow::Result<Option<Task>> {
+        match self {
+            Self::Scrape(task) => task.run().await,
+            Self::Synthesis(task) => task.run().await,
         }
     }
 }
 
-static USER_AGENT: &str = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36";
-
-pub(crate) async fn run_task(task: &Task) -> anyhow::Result<()> {
-    println!("Running task: {:?}", task);
-
-    let client = reqwest::Client::builder().user_agent(USER_AGENT).build()?;
-    let res = client.get(&task.url).send().await?;
-    if res.status() != reqwest::StatusCode::OK {
-        anyhow::bail!("Failed to fetch: {}", res.status());
-    }
-    let html = res.text().await?;
-    let html = Html::parse_document(&html);
-    let scraper = Narou;
-    let episode = scraper.scrape(html)?;
-
-    let dir = PathBuf::from("temp").join(task.id.id.to_raw());
-    if !dir.exists() {
-        std::fs::create_dir(&dir)?;
-    }
-
-    let episode_path = dir.join("episode.json");
-    let f = std::fs::File::create(&episode_path)?;
-    serde_json::to_writer_pretty(f, &episode)?;
-
-    let synthesizer = VoiceVox::new();
-    let speaker = VoiceVoxSpeaker::ZundaNormal;
-    let mut paths = vec![];
-    for (i, serif) in episode.serifs.iter().enumerate() {
-        let out = dir.join(format!("{}.wav", i));
-        println!("Synthesis: ({}) {}", out.display(), serif.text);
-        synthesizer.synthesis(&serif.text, &speaker, &out).await?;
-        paths.push(out);
-    }
-    let text_path = dir.join("text.txt");
-
-    let out = dir.join("out.wav");
-    concat_wavs(&paths, &text_path, &out).await?;
-    Ok(())
+pub(crate) struct TaskRepo {
+    pub(crate) db: Surreal<Db>,
 }
 
-pub(crate) async fn watch_tasks() {
-    loop {
-        println!("Watching tasks...");
-        let db = DB.clone();
-        let tasks: Vec<Task> = db.select("tasks").await.unwrap();
-        for task in tasks {
-            run_task(&task).await.unwrap();
-            let res: Option<Task> = db.delete(&task.id).await.unwrap();
-            println!("Deleted: {:?}", res);
+impl TaskRepo {
+    const TABLE: &'static str = "tasks";
+
+    pub(crate) fn new_id() -> RecordId {
+        RecordId::from((
+            Self::TABLE.to_string(),
+            Uuid::new_v4().as_hyphenated().to_string(),
+        ))
+    }
+
+    pub(crate) async fn list(&self) -> anyhow::Result<Vec<Task>> {
+        let tasks: Vec<Task> = self.db.select(Self::TABLE).await?;
+        Ok(tasks)
+    }
+
+    pub(crate) async fn create(&self, task: Task) -> anyhow::Result<()> {
+        self.db
+            .create::<Option<Task>>(task.id())
+            .content(task)
+            .await?;
+        Ok(())
+    }
+
+    pub(crate) async fn delete(&self, id: &RecordId) -> anyhow::Result<Task> {
+        self.db
+            .delete::<Option<Task>>(id)
+            .await?
+            .ok_or_else(|| anyhow::anyhow!("Task not found"))
+    }
+
+    pub(crate) async fn watch(&self) -> anyhow::Result<()> {
+        let mut tasks = self.list().await?;
+        if tasks.is_empty() {
+            return Ok(());
         }
-        tokio::time::sleep(Duration::from_secs(5)).await;
+        log::info!("worker | {} tasks", tasks.len());
+        for task in tasks.iter_mut() {
+            match task.run().await {
+                Ok(Some(task)) => {
+                    self.create(task).await?;
+                }
+                Err(e) => {
+                    log::error!("Failed to run task: {:?}", e);
+                }
+                _ => {}
+            };
+            let res = self.delete(&task.id()).await?;
+            log::info!("Deleted: {:?}", res.id().id.to_raw());
+        }
+        Ok(())
     }
 }
