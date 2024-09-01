41c7c7222029390b63c93f7eeeb4ae79f7c93980
wakame-tech <kamata1919@gmail.com>
2024-09-01 21:09:07 +0900

    add: setup tanstack router

---
+++ web/src/routes/__root.tsx
@@ -0,0 +1,49 @@
+import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
+import { TanStackRouterDevtools } from '@tanstack/router-devtools'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useState } from "react";
+import { trpc } from "../trpc.ts";
+import { httpBatchLink } from "@trpc/client";
+
+const Header = () => {
+    return (
+        <>
+            <div className="p-2 flex gap-2">
+                <Link to="/" className="[&.active]:font-bold">
+                    Home
+                </Link>{' '}
+            </div>
+            <hr />
+        </>
+    )
+}
+
+export const Route = createRootRoute({
+    component: () => {
+        const [queryClient] = useState(() => new QueryClient());
+        const [trpcClient] = useState(() =>
+            trpc.createClient({
+                links: [
+                    httpBatchLink({
+                        url: `${import.meta.env.VITE_API_URL}/trpc`,
+                        async headers() {
+                            return {};
+                        },
+                    }),
+                ],
+            })
+        );
+
+        return (
+            <>
+                <trpc.Provider client={trpcClient} queryClient={queryClient}>
+                    <QueryClientProvider client={queryClient}>
+                        <Header />
+                        <Outlet />
+                        <TanStackRouterDevtools />
+                    </QueryClientProvider>
+                </trpc.Provider>
+            </>
+        );
+    },
+})
--- web/vite.config.ts
+++ web/vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import UnoCSS from "unocss/vite";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), UnoCSS()],
+  plugins: [TanStackRouterVite(), react(), tsconfigPaths(), UnoCSS()],
   build: {
     outDir: "./api/dist",
   },
