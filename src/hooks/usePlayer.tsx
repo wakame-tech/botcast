272f7d56aa1fc648f64478d4d7f51a25138d64bf
wakame-tech <kamata1919@gmail.com>
2024-09-23 17:39:52 +0900

    fix: remove console.log [skip ci]

--- src/hooks/usePlayer.tsx
+++ src/hooks/usePlayer.tsx
@@ -2,53 +2,51 @@ import React, { useRef, useState } from "react";
 import ReactPlayer from "react-player";
 
 export const usePlayer = () => {
-	const ref = useRef<ReactPlayer>();
-	const [seconds, setSeconds] = useState(0);
-	const [isPlaying, setPlaying] = useState(false);
+  const ref = useRef<ReactPlayer>();
+  const [seconds, setSeconds] = useState(0);
+  const [isPlaying, setPlaying] = useState(false);
 
-	const seek = (seconds: number) => {
-		if (ref.current) {
-			ref.current.seekTo(seconds, "seconds");
-			setPlaying(true);
-		}
-	};
+  const seek = (seconds: number) => {
+    if (ref.current) {
+      ref.current.seekTo(seconds, "seconds");
+      setPlaying(true);
+    }
+  };
 
-	const play = () => {
-		setPlaying((playing) => !playing);
-	};
+  const play = () => {
+    setPlaying((playing) => !playing);
+  };
 
-	const render = (url: string) => {
-		return (
-			<ReactPlayer
-				url={url}
-				playing={isPlaying}
-				config={{
-					file: {
-						forceAudio: true,
-					},
-				}}
-				width="100%"
-				height="100%"
-				ref={(player) => {
-					if (player) {
-						ref.current = player;
-						console.log(player.state);
-					}
-				}}
-				progressInterval={300}
-				onProgress={({ playedSeconds }) => {
-					console.log(ref.current?.state);
-					setSeconds(playedSeconds);
-				}}
-			/>
-		);
-	};
+  const render = (url: string) => {
+    return (
+      <ReactPlayer
+        url={url}
+        playing={isPlaying}
+        config={{
+          file: {
+            forceAudio: true,
+          },
+        }}
+        width="100%"
+        height="100%"
+        ref={(player) => {
+          if (player) {
+            ref.current = player;
+          }
+        }}
+        progressInterval={300}
+        onProgress={({ playedSeconds }) => {
+          setSeconds(playedSeconds);
+        }}
+      />
+    );
+  };
 
-	return {
-		isPlaying,
-		play,
-		seconds,
-		seek,
-		render,
-	};
+  return {
+    isPlaying,
+    play,
+    seconds,
+    seek,
+    render,
+  };
 };
