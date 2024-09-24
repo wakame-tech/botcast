9d08fa11037c6ea86bb89f8c86b2d8a27163f5d1
wakame-tech <kamata1919@gmail.com>
2024-09-24 20:19:05 +0900

    fix: use react-hook-form

--- src/routes/podcasts/$podcastId_/new.lazy.tsx
+++ src/routes/podcasts/$podcastId_/new.lazy.tsx
@@ -1,6 +1,6 @@
-import { trpc } from "@/trpc.ts";
+import { EpisodeForm } from "@/components/episode/EpisodeForm";
+import type { Episode } from "@prisma/client";
 import { createLazyFileRoute, useNavigate } from "@tanstack/react-router";
-import { useState } from "react";
 
 export const Route = createLazyFileRoute("/podcasts/$podcastId/new")({
 	component: NewEpisode,
@@ -8,38 +8,16 @@ export const Route = createLazyFileRoute("/podcasts/$podcastId/new")({
 
 export function NewEpisode() {
 	const { podcastId } = Route.useParams();
-	const newEpisode = trpc.newEpisode.useMutation();
-	const [title, setTitle] = useState("");
-	const [url, setUrl] = useState("");
 	const navigate = useNavigate();
 
-	const handleSubmit = async () => {
-		const { episode } = await newEpisode.mutateAsync({ podcastId, title, url });
-		setTitle("");
-		setUrl("");
+	const onCreate = (episode: Episode) => {
 		navigate({ to: "/episodes/$episodeId", params: { episodeId: episode.id } });
 	};
 
 	return (
 		<>
 			<h1>New Episode</h1>
-			<input
-				type="text"
-				value={title}
-				onChange={(e) => setTitle(e.target.value)}
-				placeholder="Title"
-			/>
-			<br />
-			<input
-				type="text"
-				value={url}
-				onChange={(e) => setUrl(e.target.value)}
-				placeholder="URL"
-			/>
-			<br />
-			<button type="button" onClick={handleSubmit}>
-				Create
-			</button>
+			<EpisodeForm podcastId={podcastId} onCreate={onCreate} />
 		</>
 	);
 }
