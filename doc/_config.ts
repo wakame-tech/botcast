f2047454e10037c32fc7ac15c2fccdcde00f2b33
wakame-tech <kamata1919@gmail.com>
2024-10-25 00:08:25 +0900

    fix: ARCHITECTURE.md moved to docs

--- doc/_config.ts
+++ doc/_config.ts
@@ -1,8 +1,12 @@
+import mermaid from "@ooker777/lume-mermaid-plugin/";
 import lume from "lume/mod.ts";
 import lumocs from "lumocs/mod.ts";
 
-const site = lume();
+const site = lume({
+    src: "src",
+});
 
+site.use(mermaid());
 site.use(lumocs());
 
 export default site;
