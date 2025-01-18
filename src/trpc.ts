b49052b0af28c8b15774afc9297e3788aadab838
wakame-tech <kamata1919@gmail.com>
2025-01-18 13:59:16 +0900

    remove: comments ui

--- src/trpc.ts
+++ src/trpc.ts
@@ -1,14 +1,8 @@
 import { createTRPCReact } from "@trpc/react-query";
 import type { AppRouter } from "../api/src/router";
 export type { User } from "@prisma/client";
-export {
-	CommentInputSchema,
-	PodcastInputSchema,
-	ScriptInputSchema,
-} from "../api/src/model";
+export { PodcastInputSchema, ScriptInputSchema } from "../api/src/model";
 export type {
-	Comment,
-	CommentInput,
 	Episode,
 	Podcast,
 	PodcastInput,
